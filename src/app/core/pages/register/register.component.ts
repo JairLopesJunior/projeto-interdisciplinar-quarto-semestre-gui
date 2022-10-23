d2a176c2148d29190a79fd17fe54f84f9687078d
unknown <jair.lopes@fatec.sp.gov.br>
2022-10-22 22:42:43 -0300

    Created a new component error

--- src/app/core/pages/register/register.component.ts
+++ src/app/core/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  componentRegistration: FormGroup;
+
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  public componentForm(): void {
+    this.componentRegistration = this._fb.group({
+      name: ['',  Validators.required],
+      email: ['',  Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      password: ['',  Validators.required],
+      repeatPassword: ['',  Validators.required]
+    })
+  }
 }
